22d7cce66498c2987a1f5ebece4a39264667efde
Sam Grose <samugrose@gmail.com>
2020-08-05 13:26:38 -0700

    Added more methods, save, onerror, and onsuccess

--- public/db.js
+++ public/db.js
@@ -8,3 +8,24 @@ request.onupgradeneeded = (event) => {
     const db = event.target.result;
     db.createObjectStore("heldTransaction", {autoIncrement:true});
 };
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    };
+};
+
+request.onerror = function(event) {
+    console.log("Error: " + event.target.errorCode);
+};
+
+// if post fails
+function saveRecord(record) {
+
+    const transaction = db.transaction(["heldTransaction"], "readwrite");
+    // Accessing heldTransaction
+    const hTStore = transaction.objectStore("heldTransaction");
+  
+    hTStore.add(record);
+}
